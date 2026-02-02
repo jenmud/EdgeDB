dcf5e99fa44f25ade583e4973d3d49487c073e75
Jenda Mudron <jenmud@gmail.com>
2026-02-02 22:19:25 +1100

    Refactor DB struct comments for clarity and consistency

--- internal/store/db.go
+++ internal/store/db.go
@@ -9,33 +9,38 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-// DB extends sqlx.DB to implement the Querier interface.
-// A implementation of the Querier interface can embed this struct to inherit its methods.
+// DB extends sqlx.DB implementing additional methods used for the store..
 type DB struct {
 	*sqlx.DB
 }
 
+// Tx starts a new transaction.
 func (b *DB) Tx(ctx context.Context) (*sql.Tx, error) {
 	return b.DB.BeginTx(ctx, nil)
 }
 
+// Close closed the database.
 func (b *DB) Close() error {
 	return b.DB.Close()
 }
 
+// InsertNode inserts a new node into the store.
 func (b *DB) InsertNode(ctx context.Context, node models.Node) (models.Node, error) {
 	return models.Node{}, errors.New("not implemented")
 }
 
+// InsertEdite inserts a new edge into the store.
 func (b *DB) InsertEdge(ctx context.Context, edge models.Edge) (models.Edge, error) {
 	return models.Edge{}, errors.New("not implemented")
 }
 
+// Nodes returns all the nodes in the store.
 func (b *DB) Nodes(ctx context.Context) ([]models.Node, error) {
 	var nodes []models.Node
 	return nodes, errors.New("not implemented")
 }
 
+// Edites returns all the edges in the store.
 func (b *DB) Edges(ctx context.Context) ([]models.Edge, error) {
 	var edges []models.Edge
 	return edges, errors.New("not implemented")
