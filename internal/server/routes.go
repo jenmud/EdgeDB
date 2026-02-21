8d8f6972962bfcf2456c3afb594f81f76ce058ac
Jenda Mudron <jenmud@gmail.com>
2026-02-22 10:11:29 +1100

    moved the route and added in swagger doco

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -21,41 +21,7 @@ func (s *Server) RegisterRoutes() http.Handler {
 
 	// /api/v1/nodes?term=...&limit=1000
 	slog.Info("registered route", slog.String("route", "/api/v1/nodes"), slog.String("query-params", strings.Join([]string{"term", "limit"}, ",")))
-	mux.HandleFunc("GET /api/v1/nodes", func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-
-		term := strings.Trim(r.URL.Query().Get("term"), "\"")
-		limit := 1000
-
-		if l, err := strconv.Atoi(r.URL.Query().Get("limit")); err == nil {
-			limit = l
-		}
-
-		var (
-			nodes []models.Node
-			err   error
-		)
-
-		if term == "" {
-			nodes, err = s.store.Nodes(ctx, store.NodesArgs{Limit: limit})
-		} else {
-			args := store.NodesTermSearchArgs{Term: term, Limit: limit}
-			nodes, err = s.store.NodesTermSearch(ctx, args)
-		}
-
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusNotFound)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-
-		encoder := json.NewEncoder(w)
-		if err := encoder.Encode(nodes); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	})
+	mux.HandleFunc("GET /api/v1/nodes", s.GETNodes)
 
 	// Wrap the mux with CORS middleware
 	return s.corsMiddleware(mux)
@@ -80,4 +46,51 @@ func (s *Server) corsMiddleware(next http.Handler) http.Handler {
 		// Proceed with the next handler
 		next.ServeHTTP(w, r)
 	})
+}
+
+// GetNodes searches and return nodes
+// @Summary Search and return nodes
+// @Description Search and return nodes.
+// @Tags nodes
+// @Produce json
+// @Param term query string false "search term" default("")
+// @Param limit query int false "limit results returned" minimum(1) maximum(1000) default(1000)
+// @Success 200 {array} models.Node "List of nodes"
+// @Failure 400 "Bad request"
+// @Failure 500 "Internal server error"
+// @Router /api/v1/nodes [get]
+func (s *Server) GETNodes(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	term := strings.Trim(r.URL.Query().Get("term"), "\"")
+	limit := 1000
+
+	if l, err := strconv.Atoi(r.URL.Query().Get("limit")); err == nil {
+		limit = l
+	}
+
+	var (
+		nodes []models.Node
+		err   error
+	)
+
+	if term == "" {
+		nodes, err = s.store.Nodes(ctx, store.NodesArgs{Limit: limit})
+	} else {
+		args := store.NodesTermSearchArgs{Term: term, Limit: limit}
+		nodes, err = s.store.NodesTermSearch(ctx, args)
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	encoder := json.NewEncoder(w)
+	if err := encoder.Encode(nodes); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
