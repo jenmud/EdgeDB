95cccfd30487f431f7a620394a501d460a2cfee3
Jenda Mudron <jenmud@gmail.com>
2026-03-01 18:27:53 +1100

    added  in the edges routes

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -27,6 +27,8 @@ func (s *Server) RegisterRoutes() http.Handler {
 	slog.Info("registered route", slog.String("route", "/api/v1/nodes"), slog.String("query-params", strings.Join([]string{"term", "limit"}, ",")))
 	mux.HandleFunc("PUT /api/v1/nodes", s.PUTNodes)
 	mux.HandleFunc("GET /api/v1/nodes", s.GETNodes)
+	mux.HandleFunc("PUT /api/v1/edges", s.PUTEdges)
+	mux.HandleFunc("GET /api/v1/edges", s.GETEdges)
 
 	// Wrap the mux with CORS middleware
 	return s.corsMiddleware(mux)
@@ -148,6 +150,106 @@ func (s *Server) PUTNodes(w http.ResponseWriter, r *http.Request) {
 
 	encoder := json.NewEncoder(w)
 	if err := encoder.Encode(nodes); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+// GetEdges searches and return edges
+// @Summary Search and return edges
+// @Description Search and return edges.
+// @Tags edges
+// @Produce json
+// @Param term query string false "search term" default()
+// @Param snippetStart query string false "snippet start" default(<span class="text-red-500">)
+// @Param snippetEnd query string false "snippet start" default(</span>)
+// @Param tokens query int false "snippet tokens" minimum(1) maximum(64) default(10)
+// @Param limit query int false "limit results returned" minimum(1) default(1000)
+// @Success 200 {array} models.Edge "List of edges"
+// @Failure 400 "Bad request"
+// @Failure 500 "Internal server error"
+// @Router /api/v1/edges [get]
+func (s *Server) GETEdges(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	term := strings.Trim(r.URL.Query().Get("term"), "\"")
+	snippetStart := r.URL.Query().Get("snippetStart")
+	snippetEnd := r.URL.Query().Get("snippetEnd")
+
+	limit := 1000
+	tokens := 10
+
+	if l, err := strconv.Atoi(r.URL.Query().Get("limit")); err == nil {
+		limit = l
+	}
+
+	if s, err := strconv.Atoi(r.URL.Query().Get("tokens")); err == nil {
+		tokens = s
+	}
+
+	var (
+		edges []models.Edge
+		err   error
+	)
+
+	if term == "" {
+		edges, err = s.store.Edges(ctx, store.EdgesArgs{Limit: limit})
+	} else {
+		args := store.TermSearchArgs{Term: term, Limit: limit, SnippetStart: snippetStart, SnippetEnd: snippetEnd, SnippetTokens: tokens}
+		edges, err = s.store.EdgesTermSearch(ctx, args)
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	encoder := json.NewEncoder(w)
+	if err := encoder.Encode(edges); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+// PUTEdgesReq is a request for adding/updating one or more edges.
+type PUTEdgesReq struct {
+	Edges []models.Edge
+}
+
+// PUTEdges adds/update one or more edges
+// @Summary Add/update one or more edges.
+// @Description Add/update on or more edges.
+// @Tags edges
+// @Produce json
+// @Param nodes body PUTEdgesReq true "One or more nodes to add/update"
+// @Success 200 {array} models.Edge "List of edges"
+// @Failure 400 "Bad request"
+// @Failure 500 "Internal server error"
+// @Router /api/v1/edges [put]
+func (s *Server) PUTEdges(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	req := PUTEdgesReq{}
+	defer r.Body.Close()
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	edges, err := s.store.UpsertEdges(ctx, req.Edges...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	encoder := json.NewEncoder(w)
+	if err := encoder.Encode(edges); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
